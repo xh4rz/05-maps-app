65ce1850a56a4afe422eb182658e66f81c2cd77c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-07 02:55:41 -0500

    218. Mostrar la geolocalización del usuario en pantalla

---
+++ src/components/MapView.tsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+import { PlacesContext } from '../context';
+import { Loading } from './';
+
+export const MapView = () => {
+	const { isLoading, userLocation } = useContext(PlacesContext);
+
+	if (isLoading) {
+		return <Loading />;
+	}
+
+	return <div>{userLocation?.join(',')}</div>;
+};
