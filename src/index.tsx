a982c77bd6f94f6971f9c15e42333cf8bcc49a1b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-07 03:16:15 -0500

    219. Mostrar mapa de Mapbox

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MapsApp } from './MapsApp';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+mapboxgl.accessToken =
+	'pk.eyJ1IjoieGg0cnoiLCJhIjoiY2x2cnE5YWpkMGplMjJqbnV2dzdicW9zNiJ9.qKdD4f7Omb_wo1Si9t4HxQ';
+
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
