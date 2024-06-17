f62a378a0e3e3dbd26c8ecca70c7cd86f0c1eeda
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-17 02:20:13 -0500

    232. Mostrar la polyline en el mapa

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useReducer } from 'react';
-import { Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, Map, Marker, Popup } from 'mapbox-gl';
 import { MapContext } from './MapContext';
 import { mapReducer } from './mapReducer';
 import { PlacesContext } from '../';
@@ -73,13 +73,64 @@ export const MapProvider = ({ children }: Props) => {
 
 		const { distance, duration, geometry } = resp.data.routes[0];
 
+		const { coordinates: coords } = geometry;
+
 		let kms = distance / 1000;
 		kms = Math.round(kms * 100);
 		kms /= 100;
 
 		const minutes = Math.floor(duration / 60);
 
-		console.log({ kms, minutes });
+		const bounds = new LngLatBounds(start, start);
+
+		for (const coord of coords) {
+			const newCoord: [number, number] = [coord[0], coord[1]];
+
+			bounds.extend(newCoord);
+		}
+
+		state.map?.fitBounds(bounds, {
+			padding: 200
+		});
+
+		// Polyline
+		const sourceData: AnySourceData = {
+			type: 'geojson',
+			data: {
+				type: 'FeatureCollection',
+				features: [
+					{
+						type: 'Feature',
+						properties: {},
+						geometry: {
+							type: 'LineString',
+							coordinates: coords
+						}
+					}
+				]
+			}
+		};
+
+		if (state.map?.getLayer('RouteString')) {
+			state.map.removeLayer('RouteString');
+			state.map.removeSource('RouteString');
+		}
+
+		state.map?.addSource('RouteString', sourceData);
+
+		state.map?.addLayer({
+			id: 'RouteString',
+			type: 'line',
+			source: 'RouteString',
+			layout: {
+				'line-cap': 'round',
+				'line-join': 'round'
+			},
+			paint: {
+				'line-color': 'yellow',
+				'line-width': 3
+			}
+		});
 	};
 
 	return (
